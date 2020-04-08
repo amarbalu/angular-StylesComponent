1bd94881e951aed5e71006a00771ad0bba046392
amarbalu <inboxamarbalu@gmail.com>
2020-04-08 12:44:42 +0530

    class :ng-host ::ng-deep

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,12 +2,18 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1 class="child">1) Styles in decorator</h1> <p style="color:blue"> 2) Tag Styles</p>`,
+  template: `<h1 class="child">1) Styles in decorator</h1> 
+  <p style="color:blue"> 2) Tag Styles</p>`,
  // 2) Tag styles
   styles: [`h1 { font-family: Lato;background-color:orange;}
   
   :host{
-color:pink;
+margin:10px;
+border:10px solid ;
+  }
+  :host-context(.backg){
+    display:block;
+    background-color:red;
   }
    `]
   // 1) Component styles
